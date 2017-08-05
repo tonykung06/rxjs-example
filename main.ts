4cf575634756e801d0c23a764170ffa7ff316af7
tonykung <tonykung06@gmail.com>
2017-08-05 19:35:05 +0800

    async http request, retry and dom events

--- main.ts
+++ main.ts
@@ -1,32 +1,92 @@
-// import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
-// importing individual submodules from rxjs to reduce bundle size
-import {Observable} from "rxjs/Observable";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/filter";
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-let numbers = [1, 5, 10];
-let source = Observable.create(observer => {
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
+let click = Observable.fromEvent(button, "click");
 
-        if(index < numbers.length) {
-            setTimeout(produceValue, 250);
-        } else {
-            observer.complete();
-        }
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        });
+
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
+}
+
+function loadWithFetch(url: string) {
+    // only fetch when someone subscribes to the observable
+    return Observable.defer(() => {
+        return Observable.fromPromise(
+            fetch(url).then(r => r.json())
+        );
+    });
+}
+
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
     }
+}
 
-    produceValue();
-}).map(n => n * 2).filter(n => n > 4);
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
 
+loadWithFetch("movies.json").subscribe(renderMovies);
 
-source.subscribe(
-    value => console.log(`value: ${value}`),
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+    renderMovies,
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
+
+let circle = document.getElementById('circle');
+let mouseMoveSource = Observable.fromEvent(
+    document,
+    "mousemove"
+).filter((e: MouseEvent) => {
+    return e.clientX < 500
+}).map((e: MouseEvent) => {
+    return {
+        x: e.clientX,
+        y: e.clientY
+    };
+}).delay(200);
+
+mouseMoveSource.subscribe(value => {
+    circle.style.left = `${value.x}px`;
+    circle.style.top = `${value.y}px`;
+
+}, e => {
+    console.log(`error: ${e}`);
+}, () => {
+    console.log('complete');
+});
+
+
+
 
 
 
